ca0f2a53b68c4201a137b81269ea9858307dc998
Spencer Means <hspencermeans@gmail.com>
2026-09-15 09:32:49 -0700

    base script setup

---
+++ src/textnode.py
@@ -0,0 +1,31 @@
+from enum import Enum
+
+
+class TextType(Enum):
+    TEXT = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+
+class TextNode:
+
+    def __init__(self, text: str, text_type: TextType, url: str | None = None) -> None:
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+        super().__init__()
+
+    def __eq__(self, value: object) -> bool:
+        if isinstance(value, TextNode):
+            return (
+                self.text == value.text
+                and self.text_type == value.text_type
+                and self.url == value.url
+            )
+        return False
+
+    def __repr__(self) -> str:
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
